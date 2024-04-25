8710c0550dc54e35a525c42788a85c63779e5eac
Arie Setiadi <arie@eannovate.com>
2024-04-26 07:38:28 +0800

    implement theme switcher

--- src/components/side-bar.tsx
+++ src/components/side-bar.tsx
@@ -1,13 +1,29 @@
-import { RiMessage3Line, RiMenu3Line, RiSearch2Line } from '@remixicon/react';
+import { useTheme } from 'next-themes';
+
+import {
+  RiMessage3Line,
+  RiMenu3Line,
+  RiSearch2Line,
+  RiContrast2Line,
+  RiSunLine,
+} from '@remixicon/react';
 
 export default function SideBar() {
+  const { theme, setTheme } = useTheme();
+
   return (
-    <div className="max-w-lg">
+    <div className="max-w-lg md:max-w-sm">
       <div className="flex items-center justify-between border-b border-b-slate-100 p-5">
-        <div className="avatar">
+        <div className="avatar gap-2">
           <div className="w-12 rounded-full">
             <img src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" />
           </div>
+
+          <button
+            onClick={() => setTheme(theme == 'light' ? 'dark' : 'light')}
+            className="btn btn-circle">
+            {theme == 'light' ? <RiContrast2Line /> : <RiSunLine />}
+          </button>
         </div>
 
         <div className="flex gap-2">
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,14 @@
 import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
 import MainLayout from '@/components/layouts/main-layout';
+import { ThemeProvider } from 'next-themes';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <MainLayout>
-      <Component {...pageProps} />
-    </MainLayout>
+    <ThemeProvider>
+      <MainLayout>
+        <Component {...pageProps} />
+      </MainLayout>
+    </ThemeProvider>
   );
 }
