2bc25084352a3661764c9c02d2314dcc4c716391
Arie Setiadi <arie@eannovate.com>
2024-05-04 11:55:09 +0800

    implement new chat feature by email address

--- src/components/chats/chat-starter.tsx
+++ src/components/chats/chat-starter.tsx
@@ -1,17 +1,48 @@
-import { useState } from 'react';
+import { useState, useRef } from 'react';
+import { auth, db } from '@/apps/firebase';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { useCollection } from 'react-firebase-hooks/firestore';
+import { addDoc, collection } from 'firebase/firestore';
 
 export default function ChatStarter() {
-  const [email, setEmail] = useState<string>('');
+  const chatCollectionRef = collection(db, 'chats');
+  const closeModalButtonRef = useRef(null);
 
-  function handleStartChat(e: any) {
+  const [email, setEmail] = useState<string>('');
+  const [error, setError] = useState<string>('');
+
+  const [user] = useAuthState(auth);
+  const [chatCollection] = useCollection(chatCollectionRef);
+
+  const handleStartChat = async (e: any) => {
     e.preventDefault();
 
-    if (!email) return;
+    if (user?.email == email) {
+      setError(`You can't chat with yourself`);
+      return;
+    }
 
-    alert(`Starting chat with ${email}`);
+    if (checkChatExistance(user?.email as string, email)) {
+      setError(`You already have a chat with ${email}`);
+      return;
+    }
+
+    await addDoc(chatCollectionRef, {
+      users: [user?.email, email],
+    });
 
     setEmail('');
-  }
+    setError('');
+
+    // @ts-ignore
+    closeModalButtonRef?.current?.click();
+  };
+
+  const checkChatExistance = (userEmail: string, recipientEmail: string): boolean => {
+    return !!chatCollection?.docs?.some(
+      (chat) => chat.data().users.includes(userEmail) && chat.data().users.includes(recipientEmail),
+    );
+  };
 
   return (
     <>
@@ -31,26 +62,30 @@ export default function ChatStarter() {
           <p className="py-4">Enter your friend email address!</p>
 
           <form onSubmit={handleStartChat}>
-            <label className="input input-bordered mb-4 flex items-center gap-2">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 16 16"
-                fill="currentColor"
-                className="h-4 w-4 opacity-70">
-                <path d="M2.5 3A1.5 1.5 0 0 0 1 4.5v.793c.026.009.051.02.076.032L7.674 8.51c.206.1.446.1.652 0l6.598-3.185A.755.755 0 0 1 15 5.293V4.5A1.5 1.5 0 0 0 13.5 3h-11Z" />
-                <path d="M15 6.954 8.978 9.86a2.25 2.25 0 0 1-1.956 0L1 6.954V11.5A1.5 1.5 0 0 0 2.5 13h11a1.5 1.5 0 0 0 1.5-1.5V6.954Z" />
-              </svg>
+            <div className="mb-4">
+              <label className="input input-bordered flex items-center gap-2">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 16 16"
+                  fill="currentColor"
+                  className="h-4 w-4 opacity-70">
+                  <path d="M2.5 3A1.5 1.5 0 0 0 1 4.5v.793c.026.009.051.02.076.032L7.674 8.51c.206.1.446.1.652 0l6.598-3.185A.755.755 0 0 1 15 5.293V4.5A1.5 1.5 0 0 0 13.5 3h-11Z" />
+                  <path d="M15 6.954 8.978 9.86a2.25 2.25 0 0 1-1.956 0L1 6.954V11.5A1.5 1.5 0 0 0 2.5 13h11a1.5 1.5 0 0 0 1.5-1.5V6.954Z" />
+                </svg>
 
-              <input
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-                name="email"
-                type="email"
-                className="grow"
-                placeholder="Email"
-                required
-              />
-            </label>
+                <input
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                  name="email"
+                  type="email"
+                  className="grow"
+                  placeholder="Email"
+                  required
+                />
+              </label>
+
+              {!!error && <span className="mt-1 block text-red-500">{error}</span>}
+            </div>
 
             <button type="submit" className="btn">
               Start Chat
@@ -59,7 +94,7 @@ export default function ChatStarter() {
         </div>
 
         <form method="dialog" className="modal-backdrop">
-          <button>Close</button>
+          <button ref={closeModalButtonRef}>Close</button>
         </form>
       </dialog>
     </>
