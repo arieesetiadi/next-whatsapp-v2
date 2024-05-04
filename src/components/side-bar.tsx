2353820745992a15e7c878ffb581aca8a8560974
Arie Setiadi <arie@eannovate.com>
2024-05-04 14:39:09 +0800

    move logout button

--- src/components/side-bar.tsx
+++ src/components/side-bar.tsx
@@ -1,6 +1,6 @@
 import ChatStarter from './chats/chat-starter';
 import ChatItem from './chats/chat-item';
-import ThemeVariant from '@/enums/theme.enum';
+// import ThemeVariant from '@/enums/theme.enum';
 import { useTheme } from 'next-themes';
 import { signOut } from 'firebase/auth';
 import { auth, chatsCollectionRef } from '@/apps/firebase';
@@ -10,7 +10,7 @@ import { useCollection } from 'react-firebase-hooks/firestore';
 import { useAuthState } from 'react-firebase-hooks/auth';
 
 export default function SideBar() {
-  const { theme, setTheme } = useTheme();
+  //   const { theme, setTheme } = useTheme();
 
   const [chatCollection] = useCollection(chatsCollectionRef);
   const [user] = useAuthState(auth);
@@ -22,16 +22,24 @@ export default function SideBar() {
   return (
     <div className="max-w-lg md:max-w-sm">
       <div className="flex items-center justify-between p-5">
-        <div className="avatar gap-2">
-          <div className="w-12 rounded-full">
-            <img src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" />
+        <div className="dropdown dropdown-right">
+          <div tabIndex={0} role="button" className="avatar btn-circle gap-2">
+            <div className="w-12 rounded-full">
+              <img src={user?.photoURL ?? 'https://cdn-icons-png.flaticon.com/512/552/552848.png'} />
+            </div>
+
+            {/* <button
+                onClick={() => setTheme(theme == ThemeVariant.LIGHT ? ThemeVariant.DARK : ThemeVariant.LIGHT)}
+                className="btn btn-circle">
+                {theme == ThemeVariant.LIGHT ? <RiContrast2Line /> : <RiSunLine />}
+            </button> */}
           </div>
 
-          <button
-            onClick={() => setTheme(theme == ThemeVariant.LIGHT ? ThemeVariant.DARK : ThemeVariant.LIGHT)}
-            className="btn btn-circle">
-            {theme == ThemeVariant.LIGHT ? <RiContrast2Line /> : <RiSunLine />}
-          </button>
+          <ul tabIndex={0} className="menu dropdown-content z-[1] ml-2 w-36 rounded-box bg-base-100 p-2 shadow">
+            <li>
+              <a onClick={async () => await signOut(auth)}>Logout</a>
+            </li>
+          </ul>
         </div>
 
         <div className="flex gap-2">
@@ -39,16 +47,9 @@ export default function SideBar() {
             <RiMessage3Line />
           </button>
 
-          <div className="dropdown dropdown-right">
-            <button tabIndex={0} role="button" className="btn btn-circle">
-              <RiMenu3Line />
-            </button>
-            <ul tabIndex={0} className="menu dropdown-content z-[1] ml-2 w-52 rounded-box bg-base-100 p-2 shadow">
-              <li>
-                <a onClick={async () => await signOut(auth)}>Logout</a>
-              </li>
-            </ul>
-          </div>
+          <button className="btn btn-circle">
+            <RiMenu3Line />
+          </button>
         </div>
       </div>
 
